1f640347dc9faec5a0c7dbfce9b6c9c9f5ab79e2
Abbas <meerabbas110@gmail.com>
2023-09-19 11:24:15 +0530

    completed challenges on array

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,5 +1,16 @@
-function findMaxNumber(num) {
-  return Math.max(...num);
-}
+// function findMaxNumber(num) {
+//   return Math.max(...num);
+// }
+let a = [23, 53, 33, 12, 24];
+const findMaxNumber = (arr) => {
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+};
 
 module.exports = findMaxNumber;
