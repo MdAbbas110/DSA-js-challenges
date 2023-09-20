ac039d74aa62796fa2f95fd705362733975a058d
Abbas <meerabbas110@gmail.com>
2023-09-21 01:08:25 +0530

    solved two question on array and sum

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,29 @@
-function areAllCharactersUnique() {}
+// function areAllCharactersUnique(str) {
+//   const letters = {};
+
+//   for (let i = 0; i < str.length; i++) {
+//     const char = str[i];
+
+//     if (letters[char]) {
+//       return false;
+//     }
+//     letters[char] = true;
+//   }
+//   return true;
+// }
+
+const areAllCharactersUnique = (str) => {
+  const unique = new Set();
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (unique.has(char)) {
+      return false;
+    }
+    unique.add(char);
+  }
+  return true;
+};
 
 module.exports = areAllCharactersUnique;
