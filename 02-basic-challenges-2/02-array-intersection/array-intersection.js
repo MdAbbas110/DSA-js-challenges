443a26c0618a8e819303c29980c49431c57cc681
Abbas <meerabbas110@gmail.com>
2023-09-20 16:12:09 +0530

    solve the intersection array problem

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,24 @@
-function arrayIntersection() {}
+// function arrayIntersection(arr1, arr2) {
+//   let arrIncludes = [];
+
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr2.includes(arr1[i]) && !arrIncludes.includes(arr1[i])) {
+//       arrIncludes.push(arr1[i]);
+//     }
+//   }
+//   return arrIncludes;
+// }
+
+const arrayIntersection = (arr1, arr2) => {
+  const set1 = new Set(arr1);
+  const intersectionArr = [];
+
+  for (let num of arr2) {
+    if (set1.has(num)) {
+      intersectionArr.push(num);
+    }
+  }
+  return intersectionArr;
+};
 
 module.exports = arrayIntersection;
