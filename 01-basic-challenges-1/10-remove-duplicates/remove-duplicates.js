a673f3048a7edc84af566551e4d984c1820d791b
Abbas <meerabbas110@gmail.com>
2023-09-20 11:34:40 +0530

    basic challenges 1 is completed

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,14 @@
-function removeDuplicates() {}
+// const removeDuplicates = (arr) => Array.from(new Set(arr));
+
+const removeDuplicates = (arr) => {
+  const unique = [undefined];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!unique.includes(arr[i])) {
+      unique.push(arr[i]);
+    }
+  }
+  return unique;
+};
 
 module.exports = removeDuplicates;
