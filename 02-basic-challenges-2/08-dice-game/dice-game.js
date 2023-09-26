460eb0921233076649e4158f526efd5f99a76f3f
Abbas <meerabbas110@gmail.com>
2023-09-26 22:49:10 +0530

    code of dice game

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,29 @@
-function diceGameSimulation() {}
+function rollDice(num) {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function diceGameSimulation(simulationDice) {
+  const results = [];
+
+  for (let i = 0; i < simulationDice; i++) {
+    const dice1 = rollDice();
+    const dice2 = rollDice();
+
+    const sum = dice1 + dice2;
+
+    let result = '';
+
+    if (sum === 7 || sum === 11) {
+      result = 'Win';
+    } else if (sum === 2 || sum === 3 || sum === 12) {
+      result == 'Lose';
+    } else {
+      result = 'Roll Again';
+    }
+
+    results.push({ dice1, dice2, sum, result });
+  }
+  return results;
+}
 
 module.exports = diceGameSimulation;
