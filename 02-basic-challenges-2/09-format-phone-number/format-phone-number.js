e69c778ddac10f5c7c4457a8754238deecb3c102
Abbas <meerabbas110@gmail.com>
2023-09-30 20:39:44 +0530

    finish the challenge on format the no.

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,23 @@
-function formatPhoneNumber() {}
+// function formatPhoneNumber(numbers) {
+//   const areaCode = numbers.slice(0, 3).join('');
+//   const prefix = numbers.slice(3, 6).join('');
+//   const lineNo = numbers.slice(6).join('');
+
+//   return `(${areaCode}) ${prefix}-${lineNo}`;
+// }
+
+// const formatPhoneNumber = (num) => {
+//   const formatted = num.join('');
+
+//   return `(${formatted.substring(0, 3)}) ${formatted.substring(
+//     3,
+//     6
+//   )}-${formatted.substring(6)}`;
+// };
+
+const formatPhoneNumber = (num) =>
+  `(${num.slice(0, 3).join('')}) ${num.slice(3, 6).join('')}-${num
+    .slice(6)
+    .join('')}`;
 
 module.exports = formatPhoneNumber;
