786c1ea64dfc0fad005bc4e9a17b3ac8401eb3c9
Abbas <meerabbas110@gmail.com>
2023-10-01 11:58:16 +0530

    total sale calculator with reduce method

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,13 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(product, tax) {
+  const totalSale = product.reduce(
+    (sum, product) => sum + product.price * product.quantity,
+    0
+  );
+
+  const taxAmount = (totalSale * tax) / 100;
+  const totalSaleWithTax = totalSale + taxAmount;
+
+  return parseFloat(totalSaleWithTax.toFixed(2));
+}
 
 module.exports = calculateTotalSalesWithTax;
