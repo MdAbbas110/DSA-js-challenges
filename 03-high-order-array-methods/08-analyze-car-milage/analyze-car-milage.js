ffc05b35c147982119581dc7449082ceb7eef662
Abbas <meerabbas110@gmail.com>
2023-10-03 00:20:21 +0530

    using reduce method  properly on car array

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,24 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(car) {
+  const totalMileage = car.reduce((total, car) => total + car.mileage, 0);
+
+  const averageMileage = totalMileage / car.length;
+
+  const highestMileage = car.reduce(
+    (highest, crr) => (crr.mileage > highest.mileage ? crr : highest),
+    car[0]
+  );
+
+  const lowestMileage = car.reduce(
+    (lowest, crr) => (crr.mileage < lowest.mileage ? crr : lowest),
+    car[0]
+  );
+
+  return {
+    averageMileage: parseFloat(averageMileage.toFixed(2)),
+    highestMileage,
+    lowestMileage,
+    totalMileage,
+  };
+}
 
 module.exports = analyzeCarMileage;
