63dc391de8891d5149f441001063d245867824bf
Abbas <meerabbas110@gmail.com>
2023-10-02 15:37:40 +0530

    anagram of two words compare

--- 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
+++ 03-high-order-array-methods/05-valid-anagrams/valid-anagrams.js
@@ -1,3 +1,17 @@
-function validAnagrams() {}
+function validAnagrams(str1, str2) {
+  const freqCunt1 = str1.split('').reduce((acc, char) => {
+    acc[char] = (acc[char] || 0) + 1;
+    return acc;
+  }, {});
+
+  const freqCunt2 = str2.split('').reduce((acc, char) => {
+    acc[char] = (acc[char] || 0) + 1;
+    return acc;
+  }, {});
+
+  return Object.keys(freqCunt1).every(
+    (char) => freqCunt1[char] === freqCunt2[char]
+  );
+}
 
 module.exports = validAnagrams;
