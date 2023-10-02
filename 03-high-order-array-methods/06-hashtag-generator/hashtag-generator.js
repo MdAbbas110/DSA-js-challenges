66aa8435af57b3a80c5bc4538dbde15492535897
Abbas <meerabbas110@gmail.com>
2023-10-02 16:06:59 +0530

    adding hashtag for the string passed in

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,28 @@
-function generateHashtag() {}
+// function generateHashtag(str) {
+//   if (str.trim === '') {
+//     return false;
+//   }
+
+//   const words = str.trim().split(/\s+/);
+
+//   const capitalize = words.map(
+//     (word) => word.charAt(0).toUpperCase() + word.slice(1)
+//   );
+
+//   const hashtag = `#${capitalize.join('')}`;
+
+//   return hashtag.length > 140 ? false : hashtag;
+// }
+
+const generateHashtag = (str) => {
+  if (str.length === 1 || str.length > 140) {
+    return false;
+  }
+
+  const hashtag = str.split(' ').reduce((tag, word) => {
+    return tag + word.charAt(0).toUpperCase() + word.substring(1);
+  }, '#');
+  return hashtag;
+};
 
 module.exports = generateHashtag;
