97ccabd02e1a57b49335391d73b10b8713458c1e
Abbas <meerabbas110@gmail.com>
2023-10-02 16:53:48 +0530

    valid ip address validation

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,12 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (str) => {
+  const ip = str.split('.');
+  if (ip.length > 4) {
+    return false;
+  }
+  return ip.every((octets) => {
+    const num = parseInt(octets);
+    return num >= 0 && num <= 255 && octets === num.toString();
+  });
+};
 
 module.exports = isValidIPv4;
