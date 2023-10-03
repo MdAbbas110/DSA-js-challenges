f078531a95fa949b074da16b15f6bd7e525195ba
Abbas <meerabbas110@gmail.com>
2023-10-03 23:36:28 +0530

    added code on password checker

--- 03-high-order-array-methods/09-password-validator/password-validator-run.js
+++ 03-high-order-array-methods/09-password-validator/password-validator-run.js
@@ -1,6 +1,6 @@
 const validatePassword = require('./password-validator');
 
-const result1 = validatePassword('Abc12345');
+const result1 = validatePassword('Abc123456');
 const result2 = validatePassword('password');
 
 console.log(result1, result2);
--- 03-high-order-array-methods/09-password-validator/password-validator.js
+++ 03-high-order-array-methods/09-password-validator/password-validator.js
@@ -1,3 +1,19 @@
-function validatePassword() {}
+function validatePassword(password) {
+  const isLength = password.isLength >= 8;
+
+  const hasUppercase = password
+    .split('')
+    .some((char) => char === char.toUpperCase() && char !== char.toLowerCase());
+
+  const hasLowercase = password
+    .split('')
+    .some((char) => char === char.toLowerCase() && char !== char.toUpperCase());
+
+  const hasDigit = password
+    .split('')
+    .some((char) => !isNaN(parseInt(char, 10)));
+
+  return isLength && hasUppercase && hasLowercase && hasDigit;
+}
 
 module.exports = validatePassword;
