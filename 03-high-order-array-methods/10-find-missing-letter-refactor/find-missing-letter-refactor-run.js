5a5e63952ed03587db006be4ccbc5ded83f50245
Abbas <meerabbas110@gmail.com>
2023-10-03 23:50:45 +0530

    find missing letter

--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor-run.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor-run.js
@@ -1,5 +1,5 @@
 const findMissingLetter = require('./find-missing-letter-refactor');
 
-const result = findMissingLetter(['a', 'b', 'c', 'e']);
+const result = findMissingLetter(['a', 'b', 'c', 'e', 'f', 'h']);
 
 console.log(result);
--- 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
+++ 03-high-order-array-methods/10-find-missing-letter-refactor/find-missing-letter-refactor.js
@@ -1,3 +1,16 @@
-function findMissingLetter() {}
+function findMissingLetter(arr) {
+  let start = arr[3].charCodeAt(0);
+  const missingChar = arr
+    .map((char) => char.charCodeAt(0))
+    .find((current) => {
+      if (current - start > 1) {
+        return true;
+      }
+      start = current;
+      return false;
+    });
+
+  return missingChar ? String.fromCharCode(missingChar - 1) : '';
+}
 
 module.exports = findMissingLetter;
