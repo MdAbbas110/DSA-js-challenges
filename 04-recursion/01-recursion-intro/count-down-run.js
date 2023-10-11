0d91e9fa2fd04bb17c311134a5fa2c461171a39b
Abbas <meerabbas110@gmail.com>
2023-10-11 20:13:04 +0530

    basic of recursion and stack

--- 04-recursion/01-recursion-intro/count-down-run.js
+++ 04-recursion/01-recursion-intro/count-down-run.js
@@ -1,3 +1,4 @@
 const countDown = require('./count-down');
 
 countDown(10);
+countDown(4);
--- 04-recursion/01-recursion-intro/count-down.js
+++ 04-recursion/01-recursion-intro/count-down.js
@@ -1,3 +1,12 @@
-function countDown() {}
+function countDown(num) {
+  if (num <= 0) {
+    console.log('all Done');
+    return;
+  }
+
+  num--;
+  console.log(num);
+  countDown(num);
+}
 
 module.exports = countDown;
