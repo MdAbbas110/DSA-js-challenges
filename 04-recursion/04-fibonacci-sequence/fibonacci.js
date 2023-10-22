4c04be2a36720fd12d4dbc5c0c5a6c3f31a17516
Abbas <meerabbas110@gmail.com>
2023-10-22 21:36:06 +0530

    added code for fibonacci series

--- 04-recursion/04-fibonacci-sequence/fibonacci.js
+++ 04-recursion/04-fibonacci-sequence/fibonacci.js
@@ -1,3 +1,10 @@
-function fibonacci() {}
+function fibonacci(num) {
+  console.log(`calculating ${num}`);
+  if (num < 2) {
+    return num;
+  }
+
+  return fibonacci(num - 1) + fibonacci(num - 2);
+}
 
 module.exports = fibonacci;
