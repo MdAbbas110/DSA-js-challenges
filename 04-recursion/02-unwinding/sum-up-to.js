965e49b84ea8142a2e4e2a457d3efac23b95b414
Mohammad Abbas Abidi <109078342+MdAbbas110@users.noreply.github.com>
2023-10-24 21:31:21 +0530

    Arrow functions  sum-up-to.js

--- 04-recursion/02-unwinding/sum-up-to.js
+++ 04-recursion/02-unwinding/sum-up-to.js
@@ -6,4 +6,11 @@ function sumUpTo(num) {
   return num + sumUpTo(num - 1);
 }
 
+const sumUp = (num) => {
+    if (num === 1) {
+      return 1
+    }
+  return num + sumUp(num -1 )
+}
+
 module.exports = sumUpTo;
