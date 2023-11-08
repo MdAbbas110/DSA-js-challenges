925712f8c73a57373ff2875167ce073f5a884da6
Mohammad Abbas Abidi <109078342+MdAbbas110@users.noreply.github.com>
2023-11-08 23:44:40 +0530

    Update hashtag-generator.js by other function

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,18 +1,18 @@
-// function generateHashtag(str) {
-//   if (str.trim === '') {
-//     return false;
-//   }
+ function generateHashtag(str) {
+if (str.trim === '') {
+  return false;
+ }
 
-//   const words = str.trim().split(/\s+/);
+const words = str.trim().split(/\s+/);
 
-//   const capitalize = words.map(
-//     (word) => word.charAt(0).toUpperCase() + word.slice(1)
-//   );
+ const capitalize = words.map(
+   (word) => word.charAt(0).toUpperCase() + word.slice(1)
+ );
 
-//   const hashtag = `#${capitalize.join('')}`;
+ const hashtag = `#${capitalize.join('')}`;
 
-//   return hashtag.length > 140 ? false : hashtag;
-// }
+ return hashtag.length > 140 ? false : hashtag;
+ }
 
 const generateHashtag = (str) => {
   if (str.length === 1 || str.length > 140) {
