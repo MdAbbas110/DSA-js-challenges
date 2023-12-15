b912466f0dd3b8f80424e6660b0667a9c573cccb
Mohammad Abbas Abidi <109078342+MdAbbas110@users.noreply.github.com>
2023-12-15 23:51:45 +0530

    example of the time explainBigO.js

--- 05-complexity/02-big-o-notation/explainBigO.js
+++ 05-complexity/02-big-o-notation/explainBigO.js
@@ -1,1 +1,20 @@
 // Big O is a liner time complexcity and it is just constent
+let users = [
+  {
+    name: 'abbas',
+    kidneys: [
+      {
+        healthy: false,
+      },
+      {
+        healthy: true,
+      },
+    ],
+  },
+];
+
+const numOfHealthyKidneys = users.flatMap(user => user.kidneys) // Flatten the nested array
+                              .filter(kidney => kidney.healthy) // Filter healthy kidneys
+                              .length; // Get the count
+
+console.log(numOfHealthyKidneys); // Outputs the count of
