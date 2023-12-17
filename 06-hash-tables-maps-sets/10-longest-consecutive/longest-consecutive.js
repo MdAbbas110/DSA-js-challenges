2d1b96547583dcf87384ed6ca3b03c3179f6a8a9
Mohammad Abbas Abidi <109078342+MdAbbas110@users.noreply.github.com>
2023-12-17 21:53:11 +0530

    Update longest-consecutive.js

--- 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
+++ 06-hash-tables-maps-sets/10-longest-consecutive/longest-consecutive.js
@@ -1,3 +1,22 @@
-function longestConsecutiveSequence() {}
+function longestConsecutiveSequence(nums) {
+   const numSet = new Set(nums);
+  let longestSeq = 0;
+
+  for (let num of numSet) {
+    if (!numSet.has(num - 1)) {
+      let currentNum = num;
+      let currentSeq = 1;
+
+      while (numSet.has(currentNum + 1)) {
+        currentNum++;
+        currentSeq++;
+      }
+
+      longestSeq = Math.max(longestSeq, currentSeq);
+    }
+  }
+
+  return longestSeq;
+}
 
 module.exports = longestConsecutiveSequence;
